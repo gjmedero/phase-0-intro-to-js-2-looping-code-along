59f8ec0674f7b516803a4c231a78425757daa9d4
Gilberto Medero <gjmedero@gmail.com>
2023-03-13 01:00:56 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(gifts, eventName) {
+    const messages = [];
+    for (let index = 0; index < gifts.length; index++) {
+        messages[index] = `Thank you, ${gifts[index]}, for the wonderful surprise gift!`
+    }
+
+    return messages;
+}
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
